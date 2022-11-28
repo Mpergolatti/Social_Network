48cc3c5c9544a637966dfe75011bcdc98ad277a6
Mpergolatti <Mpergolatti@gmail.com>
2022-11-28 11:37:37 -0500

    User Schema models set

--- models/User.js
+++ models/User.js
@@ -1,1 +1,37 @@
 const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill a valid email address']
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      }
+    ]
+  }
+);
+
+const User = model('User', UserSchema);
+
+module.exports = User;
