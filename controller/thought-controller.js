f0ec75455cf2c1d2f0d825610ca388bf01b79d4d
Mpergolatti <Mpergolatti@gmail.com>
2022-12-02 10:22:38 -0500

    updated and deleted thought controllers

--- controller/thought-controller.js
+++ controller/thought-controller.js
@@ -60,6 +60,57 @@ const thoughtController = {
       })
       .catch(err => res.json(err));
   },
+
+  // Update a thought by ID
+  updateThought ({ params, body }, res) {
+    Thought.findOneAndUpdate(
+        { _id: params.id },
+        { $push: { replies: body } },
+        { new: true, runValidators: true}
+      )
+      .then(updatedThought => {
+        if (!updatedThought) {
+          res.status(404).json({ message: 'No thought found with this ID'})
+          return;
+        }
+        res.json(updatedThought);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // Delete a Thought by ID
+  deleteThought({ params, body }, res) {
+    Thought.findOneAndDelete(
+      { _id: params.id }  
+    )
+    .then(deletedThought => {
+      if (!deletedThought) {
+        res.status(404).json({ message: 'No thought found with this ID'})
+        return;
+      }
+      res.json(deletedThought);
+    })
+    .catch(err => res.json(err));
+  }
+
+  // Add Reaction
+  addReaction ({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true, runValidators: true }  
+    )
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No Thought found with this ID'})
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.json(err));
+  },
+
+  // Delete Reaction
 }
 
 module.exports = thoughtController;
