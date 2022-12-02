2c20474eb1009e9ad7554da603eaa5f6a8a3124e
Mpergolatti <Mpergolatti@gmail.com>
2022-12-02 13:38:29 -0500

    added add and remove friend functionality controllers

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -73,8 +73,38 @@ const userController = {
       res.status(400).json(err);
     });
 
-  }
+  },
 
+  // Add Friends
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+        { _id: params.userId },
+        { $push: { friends: params.friendId } },
+        { new: true, runValidators: true }
+      )
+      .then(dbUserData => {
+        
+        // If NO user is found, send 404
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this ID' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // Remove Friends
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+        { _id: params.userId },
+        { $pull: { friends: params.friendId } },
+        { new: true }
+      )
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  }
 }
+
 
 module.exports = userController;
